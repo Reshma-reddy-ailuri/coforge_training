53b886019511b601e1458bb4c3319a5dd9231ff9
Ailuri Reshma Reddy <reshmaailuri9100@gmail.com>
2026-07-15 13:09:23 +0530

    Add Day13-SpringCore untracked files

---
+++ Day13-SpringCore/Assignment/App1.java
@@ -0,0 +1,16 @@
+package com.coforge.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.coforge.model.Student;
+
+//XML
+public class App1 {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Student student = (Student) context.getBean("student");
+		System.out.println(student);
+		student.displayStudentDetails();
+	}
+}	
